38ee52ef0aa85505f169d81cbb3ebbd11ea252af
stitch cula <1132463097@qq.com>
2017-04-23 00:44:54 +0800

    user和app的role与rule

--- routes/api/v1/p.js
+++ routes/api/v1/p.js
@@ -61,14 +61,164 @@ router.put("/:uin",body({ limit: '10kb'}),async ctx=>{
     const res=await ctx.mongo.collection("p")
         .updateOne({uin:ctx.params["uin"]},{"$set":bean},{upsert:true});
 
-    ctx.body=ctx.body={result:(res.n===1&&res.ok===1)?200:404};
+    ctx.body={result:(res.result.n===1&&res.result.ok===1)?200:404};
 });
 
 router.del("/:uin",async ctx=>{
     const res=await ctx.mongo.collection("p")
         .deleteOne({uin:ctx.params["uin"]});
+    //todo:删除app名下所有user和role
 
-    ctx.body={result:(res.n===1&&res.ok===1)?200:404};
+    ctx.body={result:(res.result.n===1&&res.result.ok===1)?200:404};
+});
+
+router.post("/:uin/role",body({ limit: '10kb'}),async ctx=>{
+    vld.chk(ctx.request.body["role"]).notnull().string().least(3).most(16);
+    vld.chk(ctx.request.body["parent"]).string().least(24).most(24);
+
+    if(!ctx.request.body["parent"])
+        ctx.request.body["parent"]="_";
+
+    const bean={
+        uin:Crypto.createUin(),
+        role:ctx.request.body["role"],
+        parent:ctx.request.body["parent"],
+        owner:ctx.params["uin"],
+        timestamp:new Date().getTime()
+    };
+
+    if(ctx.request.body["role"]==="guest")
+        bean.uin="000000000000000000000000";
+
+    await ctx.mongo.collection("m")
+        .insertOne(bean);
+
+    ctx.body={result:200,uin:bean.uin}
+});
+
+router.get("/:uin/role/:role",async ctx=>{
+    const res=await ctx.mongo.collection("m")
+        .findOne({owner:ctx.params["uin"],uin:ctx.params["role"]});
+
+    if(res) {
+        ctx.body = {
+            result: 200,
+            uin:res.uin,
+            role:res.role,
+            parent:res.parent,
+            timestamp:res.timestamp,
+        };
+    }else
+        ctx.body={result:404};
+});
+
+router.put("/:uin/role/:role",body({ limit: '10kb'}),async ctx=>{
+    vld.chk(ctx.request.body["role"]).notnull().string().least(3).most(16);
+    vld.chk(ctx.request.body["parent"]).string().least(24).most(24);
+
+    const bean={
+        role:ctx.request.body["role"],
+        parent:ctx.request.body["parent"],
+    };
+
+    const res=await ctx.mongo.collection("m")
+        .updateOne({owner:ctx.params["uin"],uin:ctx.params["role"]},{"$set":bean},{upsert:true});
+
+    ctx.body={result:(res.result.n===1&&res.result.ok===1)?200:404};
+});
+
+router.del("/:uin/role/:role",async ctx=>{
+    const res=await ctx.mongo.collection("m")
+        .deleteOne({owner:ctx.params["uin"],uin:ctx.params["role"]});
+    //todo:删除role名下所有rule
+
+    ctx.body={result:(res.result.n===1&&res.result.ok===1)?200:404};
+});
+
+router.get("/:uin/role/:role/rule",async ctx=>{
+    const rules=[];
+    let role=ctx.params["role"],tmp;
+    while (true) {
+        tmp=await ctx.mongo.collection("m_rule")
+            .find({owner:role}, {fields: {_id: -1}}).toArray();
+        rules.push(...tmp);
+        tmp=await ctx.mongo.collection("m")
+            .findOne({owner:ctx.params["uin"],uin:ctx.params["role"]});
+        if(!tmp||tmp["parent"]==="_")
+            break;
+        else
+            role=tmp["parent"];
+    }
+
+    ctx.body={result:200,rules};
+});
+
+router.post("/:uin/role/:role/rule",body({ limit: '10kb'}),async ctx=>{
+    vld.chk(ctx.request.body["target"]).notnull().string();
+    vld.chk(ctx.request.body["method"]).notnull().string();
+    vld.chk(ctx.request.body["weight"]).notnull().number().least(0).most(100);
+    vld.chk(ctx.request.body["access"]).notnull().boolean();
+
+    if(await ctx.mongo.collection("m_rule")
+            .findOne({
+                owner:ctx.params["role"],
+                target:ctx.request.body["target"],
+                method:ctx.request.body["method"],
+            }))
+        throw "规则重复";
+
+    const bean={
+        uin:Crypto.createUin(),
+        target:ctx.request.body["target"],
+        method:ctx.request.body["method"],
+        weight:ctx.request.body["method"],
+        access:ctx.request.body["access"],
+        owner:ctx.params["role"],
+        timestamp:new Date().getTime()
+    };
+
+    await ctx.mongo.collection("m_rule")
+        .insertOne(bean);
+
+    ctx.body={result:200}
+});
+
+router.put("/:uin/role/:role/rule/:rule",body({ limit: '10kb'}),async ctx=>{
+    vld.chk(ctx.request.body["target"]).notnull().string();
+    vld.chk(ctx.request.body["method"]).notnull().string();
+    vld.chk(ctx.request.body["weight"]).notnull().number().least(0).most(100);
+    vld.chk(ctx.request.body["access"]).notnull().boolean();
+
+    if(await ctx.mongo.collection("m_rule")
+            .findOne({
+                owner:ctx.params["role"],
+                target:ctx.request.body["target"],
+                method:ctx.request.body["method"],
+            }))
+        throw "规则重复";
+
+    const bean={
+        target:ctx.request.body["target"],
+        method:ctx.request.body["method"],
+        weight:ctx.request.body["method"],
+        access:ctx.request.body["access"],
+    };
+
+    const res=await ctx.mongo.collection("m_rule")
+        .updateOne({owner:ctx.params["role"],uin:ctx.params["rule"]},{"$set":bean},{upsert:true});
+
+    ctx.body={result:(res.result.n===1&&res.result.ok===1)?200:404};
+});
+
+router.del("/:uin/role/:role/rule/:rule",async ctx=>{
+    const res=await ctx.mongo.collection("m_rule")
+        .deleteOne({owner:ctx.params["role"],uin:ctx.params["rule"]});
+
+    ctx.body={result:(res.result.n===1&&res.result.ok===1)?200:404};
+});
+
+router.get("/:uin/user",async ctx=>{
+    //todo:获取用户列表信息
 });
 
 module.exports = router;
--- routes/api/v1/u.js
+++ routes/api/v1/u.js
@@ -11,8 +11,6 @@ const body = require('koa-json-body');
 const router=new Router();
 
 router.post("/",body({ limit: '10kb'}),async ctx=>{
-    //vld.chk(ctx.query["p"]).notnull().string();
-    //vld.chk(ctx.query["s"]).notnull().string();
     vld.chk(ctx.request.body["nickname"]).notnull().string().least(3).most(16);
     vld.chk(ctx.request.body["email"]).notnull().string();
     vld.chk(ctx.request.body["password"]).notnull().string();
@@ -21,56 +19,82 @@ router.post("/",body({ limit: '10kb'}),async ctx=>{
         uin:Crypto.createUin(),
         nickname:Crypto.toBase64(ctx.request.body["nickname"]),
         email:ctx.request.body["email"],
-        password:ctx.request.body["description"],
-        timestamp:new Date().getTime(),
-        app:"000000000000000000000000"
+        password:ctx.request.body["password"],
+        timestamp:new Date().getTime()
     };
 
-    await ctx.mongo.collection("p")
+    await ctx.mongo.collection("u")
         .insertOne(bean);
 
     ctx.body={result:200,uin:bean.uin}
 });
 
 router.get("/:uin",async ctx=>{
-    const res=await ctx.mongo.collection("p")
+    const res=await ctx.mongo.collection("u")
         .findOne({uin:ctx.params["uin"]});
 
     if(res) {
         ctx.body = {
             result: 200,
             uin:res.uin,
-            name:Crypto.toString(res.name),
-            description:Crypto.toString(res.description),
+            nickname:Crypto.toString(res.name),
+            email:res.email,
             timestamp:res.timestamp,
-            owner:res.owner
         };
     }else
         ctx.body={result:404};
 });
 
 router.put("/:uin",body({ limit: '10kb'}),async ctx=>{
-    vld.chk(ctx.request.body["name"]).notnull().string().least(3).most(16);
-    vld.chk(ctx.request.body["secret"]).notnull().string();
-    vld.chk(ctx.request.body["description"]).string();
+    vld.chk(ctx.request.body["nickname"]).notnull().string().least(3).most(16);
+    vld.chk(ctx.request.body["email"]).notnull().string();
+    vld.chk(ctx.request.body["password"]).notnull().string();
 
     const bean={
-        name:Crypto.toBase64(ctx.request.body["name"]),
-        secret:ctx.request.body["secret"],
-        description:Crypto.toBase64(ctx.request.body["description"])
+        nickname:Crypto.toBase64(ctx.request.body["nickname"]),
+        email:ctx.request.body["email"],
+        password:ctx.request.body["password"]
     };
 
-    const res=await ctx.mongo.collection("p")
+    const res=await ctx.mongo.collection("u")
         .updateOne({uin:ctx.params["uin"]},{"$set":bean},{upsert:true});
 
-    ctx.body=ctx.body={result:(res.n===1&&res.ok===1)?200:404};
+    ctx.body={result:(res.result.n===1&&res.result.ok===1)?200:404};
 });
 
 router.del("/:uin",async ctx=>{
-    const res=await ctx.mongo.collection("p")
+    const res=await ctx.mongo.collection("u")
         .deleteOne({uin:ctx.params["uin"]});
+    //todo: 删除user名下的所有app
 
-    ctx.body={result:(res.n===1&&res.ok===1)?200:404};
+    ctx.body={result:(res.result.n===1&&res.result.ok===1)?200:404};
+});
+
+
+
+router.post("/:uin/auth/:app",async ctx=>{
+    if(!await ctx.mongo.collection("p")
+        .findOne({uin:ctx.params["app"]}))
+        throw "不存在的应用";
+
+    const bean={
+        uin:ctx.params["uin"],
+        app:ctx.params["app"],
+        role:"000000000000000000000000",
+        timestamp:new Date().getTime()
+    };
+
+    await ctx.mongo.collection("u_role")
+        .insertOne(bean);
+
+    ctx.body={result:200}
+});
+
+router.del("/:uin/auth/:app",async ctx=>{
+    const res=await ctx.mongo.collection("u_role")
+        .deleteOne({uin:ctx.params["uin"],app:ctx.params["app"]});
+
+    ctx.body={result:(res.result.n===1&&res.result.ok===1)?200:404};
 });
 
 module.exports = router;
